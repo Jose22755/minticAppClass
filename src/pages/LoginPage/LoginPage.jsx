456c60593af30f5687b51add1422eac7430a2a80
pradilla2422 <pradillajohan6@gmail.com>
2025-08-15 21:27:53 -0500

    miticPradilla

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,33 @@
+import React, { useState } from 'react';
 import './LoginPage.css'
+import Swal from "sweetalert2"
 
 function LoginPage(){
+    // codigo de js(
+
+    const [username, setUsername] = useState('');
+
+
+    function handLoginClick(){
+        if(username.trim() === ''){
+            Swal.fire("Por favor ingrese un nombre valido");
+        }
+        else{
+            Swal.fire(username);
+    }
+    }
+    // etiquetas de HTML
     return (
-        <h1>LOGIN FUNCIONANDO</h1>
+        <div className='login-container'>
+            <h2>Ejercicio de prueba</h2>
+            <input type="text" 
+                placeholder='escribe tu nombre de usuario'
+                value={username}
+                onChange={(e) =>setUsername(e.target.value)}
+            />
+            <button onClick={handLoginClick}>Iniciar Sesion</button>
+                
+        </div>
     );
 }
 
